9ee6baff031b48257a12798870ae5ac86425ba42
prof-carlos <128708765+prof-carlos@users.noreply.github.com>
2025-03-27 21:07:14 +0000

    ok

--- aula/aula/src/main/java/com/example/aula/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.aula.repository;
-
-import com.example.aula.model.UsuarioEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
-}
