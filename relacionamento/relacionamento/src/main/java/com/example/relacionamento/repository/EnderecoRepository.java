ac729a584284b4ad770f47176eda5f838f7f2104
prof-carlos <128708765+prof-carlos@users.noreply.github.com>
2025-03-27 21:11:35 +0000

    ok

--- relacionamento/relacionamento/src/main/java/com/example/relacionamento/repository/EnderecoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.relacionamento.repository;
-
-import com.example.relacionamento.model.Endereco;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
-}
